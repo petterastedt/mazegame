b252758c67515c1aa6945d0074f77060822b14c9
Petter <petter.astedt@gmail.com>
2019-01-23 14:41:03 +0100

    fixed physics, working on collision detection

--- js/levels.js
+++ js/levels.js
@@ -2,7 +2,7 @@ class Level {
     constructor() {
         this.hitDetectionArray = [
             { name: "obj1", x: 10, y: 10, w: 480, h: 40 },
-            { name: "obj2", x: 450, y: 40, w: 40, h: 90 },
+            { name: "obj2", x: 450, y: 50, w: 40, h: 80 },
             { name: "obj3", x: 350, y: 100, w: 100, h: 30 },
             { name: "obj4", x: 290, y: 130, w: 90, h: 30 },
             { name: "obj5", x: 230, y: 100, w: 90, h: 30 },
@@ -13,6 +13,8 @@ class Level {
             { name: "obj10", x: 60, y: 330, w: 430, h: 40 },
             { name: "obj11", x: 430, y: 330, w: 40, h: 160 }
           ];
+        this.collision = false
+        this.cleared = false
         //let object = this.hitDetectionArray.filter(obj => obj.name === "obj1");
     }
     drawLevel (){
@@ -28,7 +30,7 @@ class Level {
         // Intro text / reset
         if (game.player.x == 30 && game.player.y == 30) {
             winSong.pause()
-            ctx.font = '12px PressStart2P-Regular'
+            ctx.font = '11px PressStart2P-Regular'
             ctx.fillText('Level 1: Get out of the maze ',90,205)
             ctx.fillText('before the timer runs out.',90,225) 
             ctx.fillText('Remember: what seems to be easy',90,245) 
@@ -43,7 +45,7 @@ class Level {
         ctx.fillStyle = "#fed480" // level color
         // (posx,posy,size,size)
         ctx.fillRect(10,10,480,40) // top
-        ctx.fillRect(450,40,40,90) // first turn
+        ctx.fillRect(450,50,40,80) // first turn
         ctx.fillRect(350,100,100,30) // part two start
         ctx.fillRect(290,130,90,30)  // upper straight part  
         ctx.fillRect(230,100,90,30) // lower straight part
@@ -64,9 +66,40 @@ class Level {
         this.collisionDetection()
     }
     collisionDetection() {
-        let lvlObject = this.hitDetectionArray.filter(obj => obj.name === "obj1")
-        if (game.player.y + game.player.radius < lvlObject[0].h || game.player.x + game.player.radius > lvlObject[0].w) {
-            console.log('outside of box!')
+        let lvlObject = this.hitDetectionArray.filter(obj => obj.name === "obj2")
+        let lvlObject2 = this.hitDetectionArray.filter(obj => obj.name === "obj1")
+        let lvlObject3 = this.hitDetectionArray.filter(obj => obj.name === "obj3")
+
+
+        if (game.player.y - game.player.radius > lvlObject[0].y && game.player.y + game.player.radius < lvlObject[0].h + lvlObject[0].y && game.player.x + game.player.radius < lvlObject[0].w + lvlObject[0].x && game.player.x - game.player.radius > lvlObject[0].x) {
+            console.log('inside of box2')
+        }
+        if (game.player.y - game.player.radius > lvlObject2[0].y && game.player.y + game.player.radius < lvlObject2[0].h + lvlObject2[0].y && game.player.x + game.player.radius < lvlObject2[0].w + lvlObject2[0].x && game.player.x - game.player.radius > lvlObject2[0].x) {
+            console.log('inside of box1')
+        }
+        if (game.player.y - game.player.radius > lvlObject3[0].y && game.player.y + game.player.radius < lvlObject3[0].h + lvlObject3[0].y && game.player.x + game.player.radius < lvlObject3[0].w + lvlObject3[0].x && game.player.x - game.player.radius > lvlObject3[0].x) {
+            console.log('inside of box3')
         }
     }
 }
+
+// for (let i= 0; i < this.hitDetectionArray.length; i++) {
+//     console.log(this.hitDetectionArray[i]);
+// }
+// detects if user is inside a box vertically
+// if (game.player.y - game.player.radius > lvlObject[0].y && game.player.y + game.player.radius < lvlObject[0].h + lvlObject[0].y && game.player.x + game.player.radius < lvlObject[0].w + lvlObject[0].x && game.player.x - game.player.radius > lvlObject[0].x) {
+//     console.log('inside of box2')
+// }
+// if (game.player.y - game.player.radius > lvlObject2[0].y && game.player.y + game.player.radius < lvlObject2[0].h + lvlObject2[0].y && game.player.x + game.player.radius < lvlObject2[0].w + lvlObject2[0].x && game.player.x - game.player.radius > lvlObject2[0].x) {
+//     console.log('inside of box1')
+// }
+// if (game.player.y - game.player.radius > lvlObject3[0].y && game.player.y + game.player.radius < lvlObject3[0].h + lvlObject3[0].y && game.player.x + game.player.radius < lvlObject3[0].w + lvlObject3[0].x && game.player.x - game.player.radius > lvlObject3[0].x) {
+//     console.log('inside of box3')
+// }
+// detects if user is inside a box horizontally
+
+// if (param1 || param2 === false) {
+//     // if character is outside the box but inside anotherbox:
+//     // DO SOMETHING
+//     // else Game over
+// }
